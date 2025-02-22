f5f7b3e54c9fabd33e22715542c75c9d4d57852b
faizm10 <faizmustansar10@gmail.com>
2025-02-22 00:34:59 -0500

    added pages

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,7 +2,8 @@ import "@/styles/globals.css";
 import { Metadata, Viewport } from "next";
 import { Link } from "@heroui/link";
 import clsx from "clsx";
-
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/ui/app-sidebar";
 import { Providers } from "./providers";
 
 import { siteConfig } from "@/config/site";
@@ -43,21 +44,13 @@ export default function RootLayout({
       >
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
           <div className="relative flex flex-col h-screen">
-            <Navbar />
-            <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
-              {children}
-            </main>
-            <footer className="w-full flex items-center justify-center py-3">
-              <Link
-                isExternal
-                className="flex items-center gap-1 text-current"
-                href="https://heroui.com?utm_source=next-app-template"
-                title="heroui.com homepage"
-              >
-                <span className="text-default-600">Powered by</span>
-                <p className="text-primary">HeroUI</p>
-              </Link>
-            </footer>
+            <SidebarProvider>
+              <AppSidebar />
+              <main className="">
+                <SidebarTrigger />
+                {children}
+              </main>
+            </SidebarProvider>
           </div>
         </Providers>
       </body>
